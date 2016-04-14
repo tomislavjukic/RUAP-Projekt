245e815b8d8413fc5eb9a62e1c3109a25971ce91
toma <t1jukic@etfos.hr>
2016-04-14 16:50:10 +0200

    dodan pozdrav

---
+++ ConsoleApplication9/ConsoleApplication9/ConsoleApplication9.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication9.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("Nešto");
+	return 0;
+}
+
