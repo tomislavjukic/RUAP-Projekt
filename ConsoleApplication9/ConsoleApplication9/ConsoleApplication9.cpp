efac3db96bb843d55d059c04f61f43027c1e0422
toma <t1jukic@etfos.hr>
2016-04-14 16:50:10 +0200

    gotovo

--- ConsoleApplication9/ConsoleApplication9/ConsoleApplication9.cpp
+++ ConsoleApplication9/ConsoleApplication9/ConsoleApplication9.cpp
@@ -6,9 +6,13 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+<<<<<<< HEAD
 	printf("Nešto drugo");
 	printf("Pozdrav iz aplikacije od Tomislav Jukic");
 	printf("025421");
+=======
+	printf("Nešto");
+>>>>>>> 245e815... dodan pozdrav
 	return 0;
 }
 
